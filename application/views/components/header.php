cc56d67b2d4bee1fa0dc9268da46ed7919d358fb
Muhammad Hamza <muhammadhamza.webdev@gmail.com>
2025-03-04 17:41:56 +0530

    color
    theme color update

--- application/views/components/header.php
+++ application/views/components/header.php
@@ -1,59 +1,68 @@
 <div class="dashboard-header">
-            <nav class="navbar navbar-expand-lg bg-white fixed-top">
-                <a class="navbar-brand" href="index.html">
-                    <img src="https://instabarcode.com/wp-content/uploads/2023/07/logo-for-insta-b.png" width="25%" alt="">
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse " id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto navbar-right-top">
-                        
-                    
-                        <li class="nav-item dropdown nav-user">
-                            <a class="nav-link nav-user-img" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user-circle" style="font-size: 35px;
+    <nav class="navbar navbar-expand-lg bg-white fixed-top">
+        <a class="navbar-brand" href="index.html">
+            <img src="https://instabarcode.com/wp-content/uploads/2023/07/logo-for-insta-b.png" width="25%" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto navbar-right-top">
+
+
+                <li class="nav-item dropdown nav-user">
+                    <a class="nav-link nav-user-img" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false"><i class="fas fa-user-circle" style="font-size: 35px;
   display: flex;"></i></a>
-                            <div class="dropdown-menu dropdown-menu-right nav-user-dropdown" aria-labelledby="navbarDropdownMenuLink2">
-                                <div class="nav-user-info">
-                                    <h5 class="mb-0 text-white nav-user-name"><?=$this->session->userdata['loginData']['display_name']?></h5>
-                                    <span class="status"></span><span class="ml-2">Available</span>
-                                </div>
-                            
-                                <a class="dropdown-item" href="<?=base_url('logout')?>"><i class="fas fa-power-off mr-2"></i>Logout</a>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-        <!-- ============================================================== -->
-        <!-- end navbar -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- left sidebar -->
-        <!-- ============================================================== -->
-        <div class="nav-left-sidebar sidebar-dark">
-            <div class="menu-list">
-                <nav class="navbar navbar-expand-lg navbar-light">
-                    <a class="d-xl-none d-lg-none" href="#">Dashboard</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav flex-column">
-                            <li class="nav-divider">
-                                Menu
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="<?=base_url('dashboard')?>" ><i class="fa fa-fw fa-user-circle"></i>Dashboard <span class="badge badge-success">6</span></a>
-                               
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="<?=base_url('add-new-ipr')?>" ><i class="fab fa-wpforms"></i>Add New IPR</a>
-                                
-                            </li>
-                        </ul>
+                    <div class="dropdown-menu dropdown-menu-right nav-user-dropdown"
+                        aria-labelledby="navbarDropdownMenuLink2">
+                        <div class="nav-user-info">
+                            <h5 class="mb-0 text-white nav-user-name">
+                                <?=$this->session->userdata['loginData']['display_name']?></h5>
+                            <span class="status"></span><span class="ml-2">Available</span>
+                        </div>
+
+                        <a class="dropdown-item" href="<?=base_url('logout')?>"><i
+                                class="fas fa-power-off mr-2"></i>Logout</a>
                     </div>
-                </nav>
-            </div>
+                </li>
+            </ul>
         </div>
+    </nav>
+</div>
+<!-- ============================================================== -->
+<!-- end navbar -->
+<!-- ============================================================== -->
+<!-- ============================================================== -->
+<!-- left sidebar -->
+<!-- ============================================================== -->
+<div class="nav-left-sidebar sidebar-dark">
+    <div class="menu-list">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="d-xl-none d-lg-none" href="#">Dashboard</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav flex-column">
+                    <li class="nav-divider">
+                        Menu
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="<?=base_url('dashboard')?>"><i
+                                class="fa fa-fw fa-user-circle"></i>Dashboard <span
+                                class="badge badge-success">6</span></a>
+
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?=base_url('add-new-ipr')?>"><i class="fab fa-wpforms"></i>Add New
+                            IPR</a>
+
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
